2b94aa8f70fba7ef66e363cc84e1b0867a5151ae
Maureenm41 <maureenmrchl@gmail.com>
2021-01-19 10:44:02 +0100

    1.2

---
+++ 1.2.js
@@ -0,0 +1,7 @@
+var name = "Maureen"
+var lastname ="Marchal"
+var city ="Tokyo"
+<script>
+document.getElementById("demo").innerHTML =
+"I am name lastname , my dream is to visit city" + name + lastname+ city;
+    </script>

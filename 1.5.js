bfbe054591c32a2c97e174a50756bf22c5be4e4f
Maureenm41 <maureenmrchl@gmail.com>
2021-01-19 14:14:00 +0100

    1.5 test

---
+++ 1.5.js
@@ -0,0 +1,5 @@
+let decimalnumber1 = new Number(readlineSync.question('Give one decimal number')); 
+let decimalnumber2 = new Number(readlineSync.question('give one decimal number'));
+let total = decimalnumber1 + decimalnumber2;
+
+console.log('The total is' + total );

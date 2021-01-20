8cc6bad1ecf4ffc47366a45868410053377237d7
Maureenm41 <maureenmrchl@gmail.com>
2021-01-20 14:35:31 +0100

    update

--- JS 2 serie/2.1.js
+++ JS 2 serie/2.1.js
@@ -1,47 +1,15 @@
 
-var getage = prompt("How old are you?? ");
+var getage = prompt("How old are you?");
 
 
-if (getage === >18 || !isNaN(getage)){
-    while (getage!== >18 || isNaN(getage)){
-        getage = prompt("What is your favorite animal ");
+if (getage  >18 || !isNaN(getage)){
+    while (getage >18 || isNaN(getage)){
+        getage = prompt("How old are you? ");
         console.log(getage)
-       if (getage !== >18 || !isNaN(getage)){
+       if (getage  >18 || !isNaN(getage)){
            document.write("You are not yet an adult");
            break;
        }
     }
 }else {
-    document.write("You are not yet an adult");
-}
-
-
-var getage = prompt ("How old are you?");
-if(getage === >18 || !isNaN(getage))
-{while (getage!== null || isNaN(getage)){
-    getage = prompt("How old are you?");
-    console.log(getage)
-    if(getage === >18 || !isNaN(getage))
-    {
-        document.write("You are not yet an adult");
-        break;
-    }
-}
-}else {
-document.write("You are not yet an adult");
-}
-
-
-if(getage === <18 || !isNaN(getage))
-{while (getage!== <18 || isNaN(getage)){
-    getage = prompt("How old are you?");
-    console.log(getage)
-    if(getage === <18 || !isNaN(getage))
-    {
-        document.write("You are an adult");
-        break;
-    }
-}
-}else {
-document.write("You are an adult");
-}
+    document.write("You are not yet an adult");}

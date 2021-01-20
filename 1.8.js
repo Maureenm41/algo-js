f2a8a56ff94d3c10553162a39ea74362b7097e44
Maureenm41 <maureenmrchl@gmail.com>
2021-01-20 10:29:41 +0100

    1.8 work (kinda i guess)

--- 1.8.js
+++ 1.8.js
@@ -1,47 +1,48 @@
 var getanswer1 = prompt("What is your favorite animal ? ");
 
 
-if (getgetanswer1 === null || !isNaN(getgetanswer1)){
-    while (getgetanswer1!== null || isNaN(getgetanswer1)){
-        getgetanswer1 = prompt("What is your favorite animal ");
-        console.log(getgetanswer1)
-       if (getgetanswer1 !== null || !isNaN(getgetanswer1)){
-           document.write(getgetanswer1);
+if (getanswer1 === null || !isNaN(getanswer1)){
+    while (getanswer1!== null || isNaN(getanswer1)){
+        getanswer1 = prompt("What is your favorite animal ");
+        console.log(getanswer1)
+       if (getanswer1 !== null || !isNaN(getanswer1)){
+           document.write(getanswer1);
            break;
        }
     }
 }else {
-    document.write(getgetanswer1);
+    document.write(getanswer1);
 }
 
 
 var getanswer2 = prompt("Give an action");
 
 
-if (getgetanswer2 === null || !isNaN(getgetanswer2)){
-    while (getgetanswer2!== null || isNaN(getgetanswer2)){
-        getgetanswer2 = prompt("Give an action ");
-        console.log(getgetanswer2)
-       if (getgetanswer2 !== null || !isNaN(getgetanswer2)){
-           document.write(getgetanswer2);
+if (getanswer2 === null || !isNaN(getanswer2)){
+    while (getanswer2!== null || isNaN(getanswer2)){
+        getanswer2 = prompt("Give an action ");
+        console.log(getanswer2)
+       if (getanswer2 !== null || !isNaN(getanswer2)){
+           document.write(getanswer2);
            break;
        }
     }
 }else {
-    document.write(getgetanswer2);
+    document.write(getanswer2);
 }
 
 var getanswer3 = prompt("Give a place ");
 
 
-if (getgetanswer3 === null || !isNaN(getgetanswer3)){
-    while (getgetanswer3!== null || isNaN(getgetanswer3)){
-        getgetanswer3 = prompt("Give a place");
-        console.log(getgetanswer2)
-       if (getgetanswer3 !== null || !isNaN(getgetanswer3)){
-           document.write(getgetanswer3);
+if (getanswer3 === null || !isNaN(getanswer3)){
+    while (getanswer3!== null || isNaN(getanswer3)){
+        getanswer3 = prompt("Give a place");
+        console.log(getanswer2)
+       if (getanswer3 !== null || !isNaN(getanswer3)){
+           document.write(getanswer3);
            break;
        }
     }
 }else {
-    document.write(getgetanswer3);
+    document.write(getanswer3);
+}

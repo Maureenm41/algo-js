95cb7e6bcea6828cba685f9650b549248eb31700
Maureenm41 <maureenmrchl@gmail.com>
2021-01-20 14:43:02 +0100

    2.1

--- JS 2 serie/2.1.js
+++ JS 2 serie/2.1.js
@@ -2,14 +2,18 @@
 var getage = prompt("How old are you?");
 
 
-if (getage  >18 || !isNaN(getage)){
-    while (getage >18 || isNaN(getage)){
+if (getage ==18 || !isNaN(getage)){
+    while (getage <=18 || isNaN(getage)){
         getage = prompt("How old are you? ");
         console.log(getage)
-       if (getage  >18 || !isNaN(getage)){
+       if (getage  <=18 || !isNaN(getage)){
            document.write("You are not yet an adult");
            break;
        }
+       if (getage  >=18 || !isNaN(getage)){
+        document.write("You are an adult");
+        break;
+    }
     }
 }else {
     document.write("You are not yet an adult");}

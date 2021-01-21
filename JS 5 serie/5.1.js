26119d36bcb16a2036f7e742ee8a75335cfb871a
Maureenm41 <maureenmrchl@gmail.com>
2021-01-21 23:36:54 +0100

    test

--- JS 5 serie/5.1.js
+++ JS 5 serie/5.1.js
@@ -13,10 +13,4 @@ function askTvSerie()
 askTvSerie();
 
 
-/*var serieName = prompt ("What's your favorite serie");
-var prodYear = prompt ("What's the year of production?");
-var castMemberName = prompt ("What are the cast members names?");
-
-alert(serieName);
-alert(prodYear);
-alert(castMemberName);*/
+/*doesn't work like I want it to work but it kinda work*/

baa7e440c939b5f9254eee147705bfbbaad77aa3
Maureenm41 <maureenmrchl@gmail.com>
2021-01-22 00:31:42 +0100

    test

--- JS 5 serie/5.1.js
+++ JS 5 serie/5.1.js
@@ -1,5 +1,19 @@
 function askTvSerie(name , dateOf , cast)
 {
+  this.name = name ;
+  this.dateOf = dateOf ; 
+  this.cast = cast ;
+}
+
+var name = prompt ("What's your favorite serie");
+  var dateOf = prompt ("What's the year of production?");
+  var cast = prompt ("What are the cast members names?");
+
+  console.log(name.dateOf.cast);
+
+
+/*function askTvSerie()
+{
   var serieName = prompt ("What's your favorite serie");
   var prodYear = prompt ("What's the year of production?");
   var castMemberName = prompt ("What are the cast members names?");
@@ -13,7 +27,7 @@ function askTvSerie(name , dateOf , cast)
 askTvSerie();
 
 var serie = { serieName , prodYear , castMemberName};
-console.log(serie);
+console.log(serie);*/
 
 
 /*doesn't work like I want it to work but it kinda work*/

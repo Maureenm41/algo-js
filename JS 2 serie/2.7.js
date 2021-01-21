6222159b2b23a5ab4c7574ef33a04e399a07219b
Maureenm41 <maureenmrchl@gmail.com>
2021-01-21 10:28:49 +0100

    2.7 test

---
+++ JS 2 serie/2.7.js
@@ -0,0 +1,12 @@
+number1 = input("Give one number ")
+number2 = input("Give one number ")
+number3 = input("Give one number ")
+number4 = input("Give one number ")
+number5 = input("Give one number ")
+
+var x = number1 + number2
+var y = x + number3
+var z = y + number4
+var a = z + number5
+
+print(var a)

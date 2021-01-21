7fd457faf248a690f87e72be1b04cde484c6e486
Maureenm41 <maureenmrchl@gmail.com>
2021-01-21 23:17:39 +0100

    test 5.1 functions

--- JS 1 serie/1.2.js
+++ JS 1 serie/1.2.js
@@ -1,3 +1,4 @@
 var name = "Maureen";
 var lastname ="Marchal";
 var city ="Tokyo";
+console.log("I am"+ name + lastname +",my dream is to visit"+ city );
---
+++ JS 5 serie/5.1.js
@@ -0,0 +1,22 @@
+function askTvSerie()
+{
+  var serieName = prompt ("What's your favorite serie");
+  var prodYear = prompt ("What's the year of production?");
+  var castMemberName = prompt ("What are the cast members names?");
+
+   alert(serieName);
+   alert(prodYear);
+   alert(castMemberName);
+
+}
+
+askTvSerie();
+
+
+/*var serieName = prompt ("What's your favorite serie");
+var prodYear = prompt ("What's the year of production?");
+var castMemberName = prompt ("What are the cast members names?");
+
+alert(serieName);
+alert(prodYear);
+alert(castMemberName);

9bc1f400b3cb6f313ccc82571913285dab4ba209
Maureenm41 <maureenmrchl@gmail.com>
2021-01-22 11:44:05 +0100

    6.2 update

--- JS 6 serie/6.2.js
+++ JS 6 serie/6.2.js
@@ -1,4 +1,14 @@
 class Rectangle
 {
 constructor(topLeftXPos ,topLeftYPos ,width , length)
+{
+    this.topLeftXPos = topLeftXPos;
+    this.topLeftYPos = topLeftYPos ;
+    this.width = width ;
+    this.length = length ;
+}
+collides(otherRectangle)
+{
+    if 
+}
 }

a41ed0ee6298d281a4dd0e21df90bb1e1815869a
Maureenm41 <maureenmrchl@gmail.com>
2021-01-22 10:50:22 +0100

    start 6.2

--- JS 6 serie/6.1.js
+++ JS 6 serie/6.1.js
@@ -1,18 +1,31 @@
 
-/*class circle
+class Circle
 {
-    var xPos :n;
-    var yPos : n ;
-    var  radius : n;
-
-    constructor()
-    {}
+    constructor(xPos , yPos , radius)
+    {
+        this.xPos = xPos;
+        this.yPos = yPos ;
+        this.radius = radius;
+    }
     move(xOffset, yOffset)
     {
-        this.xOffset = 36 ;
-        this.yOffset = 41 ;
+        this.xPos += xOffset;
+        this.yPos += yOffset ;
     }
 
     get surface ()
-    {}
-}*/
+    {
+        return (Math.PI * Math.pow(this.radius,2));
+    }
+
+    
+}
+
+let circle= new Circle (150,400,360);
+
+console.log(circle);
+
+console.log(circle.surface);
+
+circle.move(400,150);
+console.log(circle);
